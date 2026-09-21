a665a8b2610e03449fb3090cbb7789f09791c94e
sanjoyod43-source <sanjoyod43@gmail.com>
2026-09-21 13:48:52 +0700

    initial project

---
+++ main.dart
@@ -0,0 +1,4 @@
+void main () {
+  print("hello word");
+  print("Saya Bagas Tri Sastro Sanjoyo, Senang belajar flluter yeay");
+}
---
+++ sistembilangan.dart
@@ -0,0 +1,12 @@
+void main () {
+    int decimal=10;
+    int octal=int.parse('12',radix: 8);
+    int binary=int.parse('1010',radix: 2);
+    int hexadecimal=0xA;
+
+    print("ini punya bagas tri sastro sanjoyo");
+    print(decimal);
+    print(binary);
+    print(hexadecimal);
+    print(octal);
+}
